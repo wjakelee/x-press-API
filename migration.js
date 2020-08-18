65333a7a8125a63f4bad7c41220e4fcb36f40657
wjakelee <j.wlee@hotmail.com>
2020-08-18 15:37:08 -0700

    added creation of series table

--- migration.js
+++ migration.js
@@ -11,4 +11,13 @@ db.serialize(function() {
     '`biography` TEXT NOT NULL, ' +
     '`is_currently_employed` INTEGER NOT NULL DEFAULT 1, ' +
     'PRIMARY KEY(`id`) )');
+
+  db.run('DROP TABLE IF EXISTS Series');
+
+  db.run('CREATE TABLE IF NOT EXISTS `Series` ( ' +
+    '`id` INTEGER NOT NULL, ' +
+    '`name` TEXT NOT NULL, ' +
+    '`description` TEXT NOT NULL, ' +
+    'PRIMARY KEY(`id`) )');
+  
 });
