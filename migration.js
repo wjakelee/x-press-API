d251419e376f7fffeb2b75bd411232d717c2b0de
wjakelee <j.wlee@hotmail.com>
2020-08-18 16:38:36 -0700

    added creation of issues table

--- migration.js
+++ migration.js
@@ -2,8 +2,8 @@ const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('./database.sqlite');
 
 db.serialize(function() {
+  //create Artist table
   db.run('DROP TABLE IF EXISTS Artist');
-
   db.run('CREATE TABLE IF NOT EXISTS `Artist` ( ' +
     '`id` INTEGER NOT NULL, ' +
     '`name` TEXT NOT NULL, ' +
@@ -12,12 +12,25 @@ db.serialize(function() {
     '`is_currently_employed` INTEGER NOT NULL DEFAULT 1, ' +
     'PRIMARY KEY(`id`) )');
 
+  //create Series table
   db.run('DROP TABLE IF EXISTS Series');
-
   db.run('CREATE TABLE IF NOT EXISTS `Series` ( ' +
     '`id` INTEGER NOT NULL, ' +
     '`name` TEXT NOT NULL, ' +
     '`description` TEXT NOT NULL, ' +
     'PRIMARY KEY(`id`) )');
+  
+  //create Issue table
+  db.run('DROP TABLE IF EXISTS Issue');
+  db.run('CREATE TABLE IF NOT EXISTS `Issue` ( ' +
+    '`id` INTEGER NOT NULL, ' +
+    '`name` TEXT NOT NULL, ' +
+    '`issue_number` INGETER NOT NULL, ' +
+    '`publication_date` TEXT NOT NULL, ' +
+    '`artist_id` INGETER NOT NULL, ' +
+    '`series_id` INGETER NOT NULL, ' +
+    'PRIMARY KEY(`id`), ' +
+    'FOREIGN KEY(`artist_id`) REFERENCES `Artist`(`id`), ' +
+    'FOREIGN KEY(`series_id`) REFERENCES `Series`(`id`) )');
   
 });
