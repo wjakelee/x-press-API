2a7224c0b9ddfe0f8560a030e51700894a1575f8
wjakelee <j.wlee@hotmail.com>
2020-08-18 16:23:00 -0700

    added PUT and POST handlers to upload and change series info

--- api/series.js
+++ api/series.js
@@ -6,7 +6,7 @@ const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite'
 /*for any route that has a /:seriesId parameter, this handler will be executed first
 to make sure the seriesId exists in the database and add it to the requested object*/
 seriesRouter.param('seriesId', (req, res, next, seriesId) => {
-  db.get('SELECT * FROM Series WHERE id = $seriesId', { $seriesID: seriesId },
+  db.get('SELECT * FROM Series WHERE id = $seriesId', { $seriesId: seriesId },
     (error, series) => {
       if (error){
         next(error);
@@ -31,6 +31,77 @@ seriesRouter.get('/', (req, res, next) => {
     }
   })
 });
+
+
+//GET handler used to retrieve specifc series data from the Series database using seriesId
+seriesRouter.get('/:seriesId', (req, res, next) => {
+  res.status(200).json({ series: req.series });
+});
+
+
+//POST handler adds a series to the Series database if all required parameters exist
+seriesRouter.post('/', (req, res, next) => {
+  const name = req.body.series.name;
+  const description = req.body.series.description;
+  if (!name || !description){
+    return res.sendStatus(400);               // bad request (incorrect parameters)
+  }
+
+  //insert new series into database
+  db.run(`INSERT INTO Series (name, description) VALUES ($name, $description)`,
+            {
+              $name: name,
+              $description: description
+            },
+            function(error) {
+              if (error){
+                next(error);
+              } else {
+                db.get(`SELECT * FROM Series WHERE id = ${this.lastID}`,    //retrieve last added series
+                  (error, series) => {    
+                    if (error){
+                      next(error);
+                    }
+                    res.status(201).json({ series: series });       //send last added series repsonse
+                  })
+              }
+            }
+    );
+});
+
+
+//PUT handler updates series info in the Series database
+seriesRouter.put('/:seriesId', (req, res, next) => {
+  const name = req.body.series.name;
+  const description = req.body.series.description;
+  if (!name || !description){
+    return res.sendStatus(400);               // bad request (incorrect parameters)
+  }
+
+  //update series info in database
+  db.run(`UPDATE Series SET name = $name, description = $description WHERE id = $seriesId`,
+            {
+              $name: name,
+              $description: description,
+              $seriesId: req.params.seriesId
+            },
+            function(error) {
+              if (error){
+                next(error);
+              } else {
+                db.get(`SELECT * FROM Series WHERE id = ${req.params.seriesId}`,    //retrieve last added series
+                  (error, series) => {    
+                    if (error){
+                      next(error);
+                    }
+                    res.status(200).json({ series: series });       //send last added series repsonse
+                  })
+              }
+            }
+    );
+});
+
+
 
 
 module.exports = seriesRouter;     //export series router
