ef59c9938d4e0de7411a8cf34981655e5f22ed5c
wjakelee <j.wlee@hotmail.com>
2020-08-18 13:13:25 -0700

    added GET router to artists router

--- api/artists.js
+++ api/artists.js
@@ -3,4 +3,16 @@ const artistsRouter = express.Router();    //create artists express router
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite')
 
+artistsRouter.get('/', (req, res, next) => {
+  db.all('SELECT * FROM Artist WHERE is_currently_employed = 1', (error, rows) => {
+    if (error){
+      next(error);
+    } else {
+    res.status(200).json({ artists: rows });
+    }
+  })
+})
+
+
+
 module.exports = artistsRouter;
